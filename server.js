892aeacb94e515578eec5ef9bc62b5fc0b6738c3
Hassanabdelqader <terawihassan@gmail.com>
2022-08-03 02:47:14 +0300

    update

--- server.js
+++ server.js
@@ -6,13 +6,41 @@ const myJson = require('./data/weather.json');
 
 const PORT = 3001;
 
+const KEY = process.env.MOVIE_API_KEY || `55325b5628dbe9dd987f442fdc49f072` ;
+
+const MovieURL = process.env.MOVIES_API_URL || `https://api.themoviedb.org/3/search/movie?` ;
+
+const axios = require('axios');
+
 const express = require('express');
 
 const app = express();
 
-function City(date, description) {
-    this.Date = date;
-    this.Discription = description;
+const returnedWeather = [];
+
+const returnedMovies = [];
+
+
+class weatherClass{
+	constructor(Date,Discription){
+    	this.Date=Date;
+      	this.Discription=Discription;
+
+        
+    }
+  
+}
+class moviesClass{
+	constructor(obj){
+        this.title=obj.original_title;
+        this.overview =obj.overview;
+        this.average_votes=obj.vote_average;
+        this.total_votes=obj.vote_count;
+        this.image_url =`http://image.tmdb.org/t/p/w500${obj.poster_path}`;
+        this.popularity=obj.popularity;
+        this.released_on=obj.release_date;
+    }
+  
 }
 
 app.get('/weather', (req, res) => {
@@ -22,31 +50,31 @@ app.get('/weather', (req, res) => {
       "Origin, X-Requested-With, Content-Type, Accept"
     );
 
-    const returnedWeather = [];
-    const arr = [1,2,3];
-
     let lat = req.query.lat;
     let lon = req.query.lon;
-    let searchQuery = req.query.searchQuery;
-    if (lon && lat && searchQuery) {
+  
 
-        let obj = myJson.find(element => {
-            return (searchQuery ==element.city_name)
-            
-        });
+let url =`${process.env.WEATHER_API_URL}&lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`;
+// Make a request for a user with a given ID
+getWeatherData(url,req,res);
 
-        if (obj) {
-            obj.data.forEach((element) => {
-                returnedWeather.push(new City(element.datetime, element.weather.description));
-            })
-        }
-
-        (returnedWeather.length > 0) ? res.json({'weather':returnedWeather}) : res.status(405).send('No Item Found , Please check the Lat or Lon or the City Name ');
-    } else {
-
-        res.status(404).send('Please Enter the correct Parameter (lat , Lon ,Search Query)');
-    }
 });
+
+
+app.get('/movies', (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    let querya = req.query.query;
+
+let url2 =`${MovieURL}api_key=${KEY}&query=${querya}`;
+// Make a request for a user with a given ID
+getMoviesData(url2,req,res);
+
+});
+
 
 app.get('*', (req, res) => {
 
@@ -56,6 +84,38 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
     console.log('Server listening ...');
 })
+
+let getWeatherData = async  (url,req,res)=>{
+    await axios.get(url).then((value)=>{
+
+       
+
+        value.data.data.forEach(element => {
+        let obj = new weatherClass(element.datetime , `Low of ${element.low_temp}, high of ${element.max_temp} with ${element.weather.description}`);
+        returnedWeather.push(obj);
+        });
+        res.status(200).send(returnedWeather.slice(0, 20));
+        }).catch((err)=>{
+            res.status(404).send('NO city Found with this lat and Lon');
+        })
+        
+}
+
+    
+let getMoviesData = async  (url2,req,res)=>{
+    const returnedMovies = [];
+    await axios.get(url2).then((value)=>{
+        for (let index = 0; index < Math.min(value.data.total_results||0,20); index++) {           
+            let obj = new moviesClass(value.data.results[index]);
+            returnedMovies.push(obj); 
+        }
+         res.status(200).send(returnedMovies); // if search not valid return Empty 
+       
+    }).catch((err)=>{
+        res.status(404).send('Wrong in getting data');
+    })
+   
+}
 
 
 
