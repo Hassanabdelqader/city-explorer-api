1bf87b2c8fd415ed46c783c9eb47e262e96607a8
Hassanabdelqader <terawihassan@gmail.com>
2022-08-03 15:01:16 +0300

    Update last

--- modules/movies.js
+++ modules/movies.js
@@ -27,23 +27,23 @@ class moviesClass{
 
 async function handlemovies(req, res){
 
-let querya = req.query.query;
+// let querya = req.query.query;
 
-let url2 =`${MovieURL}api_key=${KEY}&query=${querya}`;
-let url= `https://api.themoviedb.org/3/search/movie?api_key=55325b5628dbe9dd987f442fdc49f072&query=amman`;
-// Make a request for a user with a given ID
+// let url2 =`${MovieURL}api_key=${KEY}&query=${querya}`;
+// let url= `https://api.themoviedb.org/3/search/movie?api_key=55325b5628dbe9dd987f442fdc49f072&query=amman`;
+// // Make a request for a user with a given ID
 
-await axios.get(url).then((value)=>{
-    for (let index = 0; index < Math.min(value.data.total_results||0,20); index++) {           
-        let obj = new moviesClass(value.data.results[index]);
-        returnedMovies.push(obj); 
-    }
-     res.status(200).send(returnedMovies); // if search not valid return Empty 
+// await axios.get(url).then((value)=>{
+//     for (let index = 0; index < Math.min(value.data.total_results||0,20); index++) {           
+//         let obj = new moviesClass(value.data.results[index]);
+//         returnedMovies.push(obj); 
+//     }
+//      res.status(200).send(returnedMovies); // if search not valid return Empty 
    
-}).catch((err)=>{
-    res.status(404).send('Wrong in getting data');
-})
-
+// }).catch((err)=>{
+//     res.status(404).send('Wrong in getting data');
+// })
+res.status(200).send('Movies Work');
 };
 
 
