19eeaea03213549d2384927014f31948c84a1fd7
Hassanabdelqader <terawihassan@gmail.com>
2022-08-03 03:36:06 +0300

    update the H

--- server.js
+++ server.js
@@ -43,6 +43,18 @@ class moviesClass{
   
 }
 
+app.get('/', (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+
+  console.log('hi');
+  res.send('Hi');
+
+});
+
 app.get('/weather', (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header(
